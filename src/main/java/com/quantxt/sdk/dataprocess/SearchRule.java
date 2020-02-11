ce6ad2eec6d7784346920f843b732ebf8fc68b51
Matin Kamali <matin_kamali@yahoo.com>
2020-02-11 15:59:45 -0500

    support all rules for test search via sdk

--- src/main/java/com/quantxt/sdk/dataprocess/SearchRule.java
+++ src/main/java/com/quantxt/sdk/dataprocess/SearchRule.java
@@ -6,10 +6,16 @@ import com.quantxt.sdk.exception.QTApiException;
 
 import java.io.Serializable;
 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchRule implements Serializable {
+
+    public enum SearchMode {ORDERED_SPAN, FUZZY_ORDERED_SPAN, SPAN, FUZZY_SPAN, PARTIAL_SPAN,
+        PARTIAL_FUZZY_SPAN};
+
+    public enum AnalyzeMode {EXACT, EXACT_CI, WHITESPACE, SIMPLE, STANDARD, STEM};
 
     private static final long serialVersionUID = -6238012691404059856L;
 
@@ -18,15 +24,43 @@ public class SearchRule implements Serializable {
     private String phraseMatchingPattern;
     private String [] phraseMatchingGroups;
 
+    private SearchMode searchMode;
+    private AnalyzeMode analyzeMode;
+
+    private String skipPatternBetweenKeyAndValue;
+    private String skipPatternBetweenValues;
+
+    private String [] stopwordList;
+    private String [] synonymList;
+
     @JsonCreator
     public SearchRule(@JsonProperty("vocabPath") String vocabPath,
                       @JsonProperty("vocabValueType") String vocabValueType,
                       @JsonProperty("vocabRegex") String phraseMatchingPattern,
-                      @JsonProperty("vocabRegexGroups") String [] phraseMatchingGroupStr) {
+                      @JsonProperty("vocabRegexGroups") String [] phraseMatchingGroupStr,
+                      @JsonProperty("skipPatternBetweenKeyAndValue") String skipPatternBetweenKeyAndValue,
+                      @JsonProperty("skipPatternBetweenValues") String skipPatternBetweenValues,
+                      @JsonProperty("searchMode") String searchMode,
+                      @JsonProperty("analyzeMode") String analyzeMode,
+                      @JsonProperty("stopwordList") String [] stopwordList,
+                      @JsonProperty("synonymList") String [] synonymList)
+    {
         this.vocabPath = vocabPath;
         this.vocabValueType = vocabValueType;
         this.phraseMatchingPattern = phraseMatchingPattern;
         this.phraseMatchingGroups = phraseMatchingGroupStr;
+        this.skipPatternBetweenKeyAndValue = skipPatternBetweenKeyAndValue;
+        this.skipPatternBetweenValues = skipPatternBetweenValues;
+
+        if (searchMode != null) {
+            this.searchMode = SearchMode.valueOf(searchMode);
+        }
+        if (analyzeMode != null) {
+            this.analyzeMode = AnalyzeMode.valueOf(analyzeMode);
+        }
+
+        this.stopwordList = stopwordList;
+        this.synonymList = synonymList;
     }
 
     public SearchRule(Dictionary dictionary) {
@@ -40,12 +74,43 @@ public class SearchRule implements Serializable {
         this.vocabValueType = type.toString();
     }
 
-    public SearchRule(Dictionary dictionary, DataProcessCreator.DictionaryType type, String regex, String [] groups) {
+    public SearchRule(Dictionary dictionary, DataProcessCreator.DictionaryType type,
+                      String regex, String [] groups) {
         this.validateDictionary(dictionary);
         this.vocabPath = dictionary.getKey();
         this.vocabValueType = type.toString();
         this.phraseMatchingPattern = regex;
         this.phraseMatchingGroups = groups;
+    }
+
+    public SearchRule setSkipPatternBetweenKeyAndValue(String skipPatternBetweenKeyAndValue){
+        this.skipPatternBetweenKeyAndValue = skipPatternBetweenKeyAndValue;
+        return this;
+    }
+
+    public SearchRule setSkipPatternBetweenValues(String skipPatternBetweenValues){
+        this.skipPatternBetweenValues = skipPatternBetweenValues;
+        return this;
+    }
+
+    public SearchRule setSearchMode(String searchMode){
+        this.searchMode = SearchMode.valueOf(searchMode);
+        return this;
+    }
+
+    public SearchRule setAnlyzeMode(String analyzeMode){
+        this.analyzeMode = AnalyzeMode.valueOf(analyzeMode);
+        return this;
+    }
+
+    public SearchRule setStopwordList(String [] stopwordList){
+        this.stopwordList = stopwordList;
+        return this;
+    }
+
+    public SearchRule setSynonymList(String [] synonymList){
+        this.synonymList = synonymList;
+        return this;
     }
 
     private void validateDictionary(Dictionary dictionary) {
